62af7fab4b07b2dcd8ca8bc4ddbd8b9233c5532b
YJ <yair.yoseph@gmail.com>
2018-09-27 21:01:30 -0700

    Heres some more

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,48 +3,48 @@ var friends = [
         name: "Vernie",
         photo: "https://memegenerator.net/img/instances/62594154.jpg",
         scores: [
-            5,
             1,
-            4,
-            4,
-            5,
             1,
-            2,
-            5,
-            4,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
             1
         ]
     },
     {
-        name: "Yair",
+        name: "Greg",
         photo: "https://i.ytimg.com/vi/jJusJGreqTo/maxresdefault.jpg",
         scores: [
             2,
-            1,
-            4,
-            4,
-            3,
-            1,
-            1,
-            5,
             2,
-            1
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
         ]
     },
     {
         name: "kevin",
         photo: "https://pics.me.me/ugghhh-its-kevin-16246071.png",
         scores: [
-            2,
-            1,
-            4,
             3,
-            1,
-            4,
-            1,
-            5,
             3,
-            2
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
         ]
     }
 ];
